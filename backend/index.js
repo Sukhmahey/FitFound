4f6e184f12c863a9355268da476f801423b8d46c
sindyvaness <sindyvaness@gmail.com>
2025-05-23 10:47:41 -0700

    Setting MongoDB connection.

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,12 @@
 const express = require("express");
 const cors = require("cors");
 
+// Init 1. for MongoDb connection
+const dotenv = require('dotenv');
+dotenv.config();
+require('./models/db');
+// End 1.
+
 const app = express();
 app.use(cors());
 app.use(express.json());
