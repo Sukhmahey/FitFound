1bd5de244b15f01df91a8cd95c2c2d6ae943c24c
sindyvaness <sindyvaness@gmail.com>
2025-06-03 13:16:56 -0700

    Login + Signup UI: HTML structure has been built (LinkedIn - Google - Email) Unauthorized Page: HTML structure has been built.

---
+++ frontend/src/pages/auth/Login.jsx
@@ -0,0 +1,34 @@
+const Login = () => {
+    return <div>
+        <h1>Login</h1>
+
+        <div>
+            <div>
+                <button>Continue with LinkedIn</button>
+                <button>Continue with Google</button>
+            </div>
+            <span>or Continue with Email</span>
+            <div>
+                <input type="email" name="" id="" placeholder="Email"/>
+                <input type="password" name="" id="" placeholder="Password"></input>
+                <div>
+                    <button>Login</button>
+                    <button>Sign in</button>
+                    <a href="">I forgot my password</a>              
+                </div>
+            </div>
+           
+        </div>
+    </div>
+};
+
+export default Login;
+
+/*
+TODO: 
+1. LinkedIn implementation
+2. Google implementation
+3. check data in BD
+4. Redirect to the user page
+
+*/
