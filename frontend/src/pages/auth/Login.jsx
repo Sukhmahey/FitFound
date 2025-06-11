ba90c1862f0556a0cdcb2256efe29938605cf79f
Sindy Montano <sindymontano@Sindys-MacBook-Air.local>
2025-06-11 16:42:02 -0700

    Configuring Firebase app and keys

--- frontend/src/pages/auth/Login.jsx
+++ frontend/src/pages/auth/Login.jsx
@@ -1,9 +1,33 @@
-import { signInWithEmailAndPassword, auth } from 'firebase/auth';
+import { useState } from 'react';
+import axios from 'axios';
+import { signInWithEmailAndPassword } from 'firebase/auth';
+import { auth } from '../../services/firebase';
 
 const Login = () => {
 
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+
     const handleLogin = () => {
-    
+
+        signInWithEmailAndPassword(auth, email, password)
+        .then((userCredential) => {
+            return userCredential.user.getIdToken();
+        })
+        .then((idToken) => {
+            return axios.post('http://localhost:5000/api/auth/firebase-login', {}, {
+                headers: {
+                    Authorization: `Bearer ${idToken}`,
+                },
+            });
+        })
+        .then((response) => {
+            console.log('Usuario verificado:', response.data);
+        })
+        .catch((error) => {
+            console.error('Error al iniciar sesión:', error.message);
+        });
+
      
     };
 
--- frontend/src/services/firebase.js
+++ frontend/src/services/firebase.js
@@ -2,10 +2,10 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: process.env.FIREBASE_API_KEY,
-  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.FIREBASE_PROJECT_ID,
-  appId: process.env.FIREBASE_APP_ID,
+  apiKey: process.env.REACT_APP_FB_API_KEY,
+  authDomain: process.env.REACT_APP_FB_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FB_PROJECT_ID,
+  appId: process.env.REACT_APP_FB_APP_ID,
 };
 
 const app = initializeApp(firebaseConfig);
