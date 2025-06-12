1489927e05088df3a0517d4637f465f0f2965e97
Sindy Montano <sindymontano@Sindys-MacBook-Air.local>
2025-06-12 10:53:37 -0700

    Starting Firebase-admin backend config for user login

--- frontend/src/pages/auth/Login.jsx
+++ frontend/src/pages/auth/Login.jsx
@@ -1,34 +1,40 @@
 import { useState } from 'react';
+import { useNavigate } from "react-router-dom";
 import axios from 'axios';
 import { signInWithEmailAndPassword } from 'firebase/auth';
 import { auth } from '../../services/firebase';
 
 const Login = () => {
+    const navigate = useNavigate();
 
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
 
-    const handleLogin = () => {
+    const handleLoginSubmit = () => {
 
         signInWithEmailAndPassword(auth, email, password)
         .then((userCredential) => {
             return userCredential.user.getIdToken();
         })
         .then((idToken) => {
-            return axios.post('http://localhost:5000/api/auth/firebase-login', {}, {
+            return axios.post('http://localhost:3001/api/login', {}, {
                 headers: {
                     Authorization: `Bearer ${idToken}`,
                 },
             });
         })
         .then((response) => {
-            console.log('Usuario verificado:', response.data);
+            console.log('User logged in', response.data);
         })
         .catch((error) => {
-            console.error('Error al iniciar sesión:', error.message);
+            console.error('Error in login', error.message);
         });
 
      
+    };
+
+    const handleSignupClick = () => {
+        navigate("/signup");
     };
 
 
@@ -41,13 +47,18 @@ const Login = () => {
                 <button>Continue with Google</button>
             </div>
             <span>or Continue with Email</span>
-            <form>
+            <form onSubmit={ handleLoginSubmit }>
                 <div>
-                    <input type="email" name="" id="" placeholder="Email"/>
-                    <input type="password" name="" id="" placeholder="Password"></input>
+                    <input type="email" name="" id="" placeholder="Email" required
+                        value={ email } 
+                        onChange={(e) => setEmail(e.target.value)} />
+
+                    <input type="password" name="" id="" placeholder="Password" required
+                        value={ password }
+                        onChange={(e) => setPassword(e.target.value)} />
                     <div>
-                        <button>Login</button>
-                        <button>Sign in</button>
+                        <input type="submit" value="Login" />
+                        <button onClick={ handleSignupClick }>Sign in</button>
                         <a href="">I forgot my password</a>              
                     </div>
                 </div>
@@ -60,7 +71,7 @@ export default Login;
 
 /*
 TODO: 
-1. LinkedIn implementation
+
 2. Google implementation
 3. check data in BD
 4. Redirect to the user page
