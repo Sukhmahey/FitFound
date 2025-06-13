c02b18de9ab27c6944aabd1418fc1555b59c9cfc
Sindy Montano <sindymontano@Sindys-MacBook-Air.local>
2025-06-12 17:35:00 -0700

    Changing the port and starting the api.js

--- backend/index.js
+++ backend/index.js
@@ -21,5 +21,5 @@ app.get("/", (req, res) => {
   res.send("API is running...");
 });
 
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
