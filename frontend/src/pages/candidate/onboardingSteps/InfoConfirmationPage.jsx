2e3c2c9a6b0f85ca8ad41f4aecf7a28216758ffe
Hitin Sachdeva <140743627+HitinSachdeva161@users.noreply.github.com>
2025-06-28 15:30:50 -0700

    Update InfoConfirmationPage.jsx

--- frontend/src/pages/candidate/onboardingSteps/InfoConfirmationPage.jsx
+++ frontend/src/pages/candidate/onboardingSteps/InfoConfirmationPage.jsx
@@ -214,7 +214,7 @@ export default function InfoConfirmationPage({ data }) {
           <Box display="flex" flexDirection="column" gap={2} border="1px solid #ccc" borderRadius={2} p={2} mb={2}>
             <Box fullWidth display="flex" flexDirection="row" gap={2}>
               <TextField fullWidth label="First Name" value={form.personalInfo.firstName} onChange={e => handleNestedChange('personalInfo', 'firstName', e.target.value)} />
-              <TextField  fullWidth label="Middle Name" value={form.personalInfo.middleName} onChange={e => handleNestedChange('personalInfo', 'middleName', e.target.value)} />
+              <TextField fullWidth label="Middle Name" value={form.personalInfo.middleName} onChange={e => handleNestedChange('personalInfo', 'middleName', e.target.value)} />
               <TextField fullWidth label="Last Name" value={form.personalInfo.lastName} onChange={e => handleNestedChange('personalInfo', 'lastName', e.target.value)} />
             </Box>
             <TextField type="email" label="Email" value={form.personalInfo.email} onChange={e => handleNestedChange('personalInfo', 'email', e.target.value)} />
@@ -335,9 +335,9 @@ export default function InfoConfirmationPage({ data }) {
 
             {form.workExperience.map((we, i) => (
               <Box key={i} border="1px solid #ccc" borderRadius={2} p={2} mb={2}>
-                                <Box display="flex" flexDirection="column" gap={3}>
+                <Box display="flex" flexDirection="column" gap={3}>
 
-                  
+
                   <TextField fullWidth label="Company Name" value={we.companyName} onChange={e => {
                     const updated = [...form.workExperience];
                     updated[i].companyName = e.target.value;
@@ -424,10 +424,10 @@ export default function InfoConfirmationPage({ data }) {
 
           {/* Job Preference */}
           <Divider textAlign="left">Job Preference</Divider>
-<Box border="1px solid #ccc" borderRadius={2} p={2} mb={2}>
-  
+          <Box border="1px solid #ccc" borderRadius={2} p={2} mb={2}>
+
             <Box display="flex" flexDirection="column" gap={3} border="1px solid #ccc" borderRadius={2} p={2} mb={2}>
-  
+
               <h6>Desired Job Titles</h6>
               {form.jobPreference.desiredJobTitle.map((title, i) => (
                 <TextField
@@ -456,9 +456,9 @@ export default function InfoConfirmationPage({ data }) {
                 }
               }))}>+ Add Desired Job Title</Button>
             </Box>
-  
+
             <Box display="flex" flexDirection="column" gap={3}>
-  
+
               <h4>Salary Expectation</h4>
               <TextField
                 label="Minimum Salary"
@@ -478,78 +478,39 @@ export default function InfoConfirmationPage({ data }) {
                   }))
                 }
               />
-              {/* <FormControlLabel
-                control={
-                  <Checkbox
-                    checked={form.jobPreference.salaryExpectation.perHour}
-                    onChange={e =>
-                      setForm(prev => ({
-                        ...prev,
-                        jobPreference: {
-                          ...prev.jobPreference,
-                          salaryExpectation: {
-                            ...prev.jobPreference.salaryExpectation,
-                            perHour: e.target.checked
-                          }
-                        }
-                      }))
-                    }
-                  />
-                }
-                label="Per Hour"
-              />
-              <FormControlLabel
-                control={
-                  <Checkbox
-                    checked={form.jobPreference.salaryExpectation.perYear}
-                    onChange={e =>
-                      setForm(prev => ({
-                        ...prev,
-                        jobPreference: {
-                          ...prev.jobPreference,
-                          salaryExpectation: {
-                            ...prev.jobPreference.salaryExpectation,
-                            perYear: e.target.checked
-                          }
-                        }
-                      }))
-                    }
-                  />
-                }
-                label="Per Year"
-              /> */}
-              <FormControl component="fieldset">
-  <InputLabel shrink>Salary Basis</InputLabel>
-  <RadioGroup
-    value={
-      form.jobPreference.salaryExpectation.perHour
-        ? 'perHour'
-        : form.jobPreference.salaryExpectation.perYear
-        ? 'perYear'
-        : ''
-    }
-    onChange={(e) => {
-      const value = e.target.value;
-      setForm((prev) => ({
-        ...prev,
-        jobPreference: {
-          ...prev.jobPreference,
-          salaryExpectation: {
-            ...prev.jobPreference.salaryExpectation,
-            perHour: value === 'perHour',
-            perYear: value === 'perYear'
-          }
-        }
-      }));
-    }}
-  >
-    <FormControlLabel value="perHour" control={<Radio />} label="Per Hour" />
-    <FormControlLabel value="perYear" control={<Radio />} label="Per Year" />
-  </RadioGroup>
-</FormControl>
 
-  
-  
+              <FormControl component="fieldset">
+                <InputLabel shrink>Salary Basis</InputLabel>
+                <RadioGroup
+                  value={
+                    form.jobPreference.salaryExpectation.perHour
+                      ? 'perHour'
+                      : form.jobPreference.salaryExpectation.perYear
+                        ? 'perYear'
+                        : ''
+                  }
+                  onChange={(e) => {
+                    const value = e.target.value;
+                    setForm((prev) => ({
+                      ...prev,
+                      jobPreference: {
+                        ...prev.jobPreference,
+                        salaryExpectation: {
+                          ...prev.jobPreference.salaryExpectation,
+                          perHour: value === 'perHour',
+                          perYear: value === 'perYear'
+                        }
+                      }
+                    }));
+                  }}
+                >
+                  <FormControlLabel value="perHour" control={<Radio />} label="Per Hour" />
+                  <FormControlLabel value="perYear" control={<Radio />} label="Per Year" />
+                </RadioGroup>
+              </FormControl>
+
+
+
               <FormControl fullWidth>
                 <InputLabel>Job Type</InputLabel>
                 <Select
@@ -568,7 +529,7 @@ export default function InfoConfirmationPage({ data }) {
                 </Select>
               </FormControl>
             </Box>
-</Box>
+          </Box>
           <Box mt={4}>
             <Button variant="contained" color="primary" onClick={submitForm}>
               Submit
@@ -580,330 +541,4 @@ export default function InfoConfirmationPage({ data }) {
   );
 
 
-
-  //   return (
-  //     <Container sx={"mt-5"}>
-  //       <Box
-  //                 component="form"
-  //                 sx={{ '& > :not(style)': { m: 1, width: '25ch' } }}
-  //                 noValidate
-  //                 autoComplete="off"
-  //               ></Box>
-  //       <h2>Candidate Form</h2>
-
-
-  //         <TextField value={form.personalInfo.firstName} name='First Name' id="outlined-basic" label="First Name" variant="outlined" onChange={e => handleNestedChange('personalInfo', 'firstName', e.target.value)} />
-
-
-
-  //         <input value={form.personalInfo.lastName} onChange={e => handleNestedChange('personalInfo', 'lastName', e.target.value)} />
-
-
-
-  //         <input type="email" value={form.personalInfo.email} onChange={e => handleNestedChange('personalInfo', 'email', e.target.value)} />
-
-
-
-  //         <input value={form.basicInfo.phoneNumber} onChange={e => handleNestedChange('basicInfo', 'phoneNumber', e.target.value)} />
-
-
-
-  //         <select value={form.personalInfo.currentStatus} onChange={e => handleNestedChange('personalInfo', 'currentStatus', e.target.value)}>
-  //           <option value="">Select status</option>
-  //           <option value="Student">Student</option>
-  //           <option value="Working Professional">Working Professional</option>
-  //           <option value="Unemployed">Unemployed</option>
-  //           <option value="Other">Other</option>
-  //         </select>
-
-  //     <label>Language:
-  //   <input value={form.basicInfo.language} onChange={e => handleNestedChange('basicInfo', 'language', e.target.value)} />
-  // </label><br />
-
-  //       <label>Specialization:
-  //         <input value={form.personalInfo.specialization} onChange={e => handleNestedChange('personalInfo', 'specialization', e.target.value)} />
-  //       </label><br />
-
-  //       <label>Bio:
-  //         <textarea value={form.basicInfo.bio} onChange={e => handleNestedChange('basicInfo', 'bio', e.target.value)} />
-  //       </label><br />
-  //       <label>Additional Info:
-  //   <textarea value={form.basicInfo.additionalInfo} onChange={e => handleNestedChange('basicInfo', 'additionalInfo', e.target.value)} />
-  // </label><br />
-
-
-  //       <label>Skills:</label><br />
-  //       {form.skills.map((skill, i) => (
-  //         <input key={i} value={skill} onChange={e => handleArrayChange('skills', i, e.target.value)} />
-  //       ))}
-  //       <button type="button" onClick={() => addToArray('skills')}>+ Add Skill</button><br />
-  //       <fieldset>
-  //   <legend>Education</legend>
-  //   {form.education.map((edu, i) => (
-  //     <div key={i} style={{ border: '1px solid #ccc', padding: '1rem', marginBottom: '1rem' }}>
-  //       <label>Institute Name:
-  //         <input value={edu.instituteName} onChange={e => {
-  //           const updated = [...form.education];
-  //           updated[i].instituteName = e.target.value;
-  //           setForm({ ...form, education: updated });
-  //         }} />
-  //       </label><br />
-  //       <label>Credentials (e.g., BCA):
-  //         <input value={edu.credentials} onChange={e => {
-  //           const updated = [...form.education];
-  //           updated[i].credentials = e.target.value;
-  //           setForm({ ...form, education: updated });
-  //         }} />
-  //       </label><br />
-  //       <label>Start Date:
-  //         <input type="month" value={edu.startDate} onChange={e => {
-  //           const updated = [...form.education];
-  //           updated[i].startDate = e.target.value;
-  //           setForm({ ...form, education: updated });
-  //         }} />
-  //       </label><br />
-  //       <label>End Date:
-  //         <input type="month" value={edu.endDate} onChange={e => {
-  //           const updated = [...form.education];
-  //           updated[i].endDate = e.target.value;
-  //           setForm({ ...form, education: updated });
-  //         }} />
-  //       </label>
-  //     </div>
-  //   ))}
-  //   <button type="button" onClick={() => {
-  //     setForm(prev => ({
-  //       ...prev,
-  //       education: [
-  //         ...prev.education,
-  //         {
-  //           instituteName: '',
-  //           credentials: '',
-  //           startDate: '',
-  //           endDate: ''
-  //         }
-  //       ]
-  //     }));
-  //   }}>+ Add Education</button>
-  // </fieldset>
-
-  //       <fieldset>
-  //         <legend>Work Experience</legend>
-  //         {form.workExperience.map((we, i) => (
-  //           <div key={i} style={{ border: '1px solid #ccc', padding: '1rem', marginBottom: '1rem' }}>
-  //             <label>Company Name:
-  //               <input value={we.companyName} onChange={e => {
-  //                 const updated = [...form.workExperience];
-  //                 updated[i].companyName = e.target.value;
-  //                 setForm({ ...form, workExperience: updated });
-  //               }} />
-  //             </label><br />
-  //             <label>Role:
-  //               <input value={we.role} onChange={e => {
-  //                 const updated = [...form.workExperience];
-  //                 updated[i].role = e.target.value;
-  //                 setForm({ ...form, workExperience: updated });
-  //               }} />
-  //             </label><br />
-  //             <label>Job Title:
-  //   <input value={we.jobTitle} onChange={e => {
-  //     const updated = [...form.workExperience];
-  //     updated[i].jobTitle = e.target.value;
-  //     setForm({ ...form, workExperience: updated });
-  //   }} />
-  // </label><br />
-
-  // <label>Experience Level:
-  //   <select value={we.experienceLevel} onChange={e => {
-  //     const updated = [...form.workExperience];
-  //     updated[i].experienceLevel = e.target.value;
-  //     setForm({ ...form, workExperience: updated });
-  //   }}>
-  //     <option value="">Select level</option>
-  //     <option value="junior">Junior</option>
-  //     <option value="middle">Middle</option>
-  //     <option value="senior">Senior</option>
-  //   </select>
-  // </label><br />
-
-  // <label>Remark from Employer:
-  //   <textarea value={we.remarkFromEmployer} onChange={e => {
-  //     const updated = [...form.workExperience];
-  //     updated[i].remarkFromEmployer = e.target.value;
-  //     setForm({ ...form, workExperience: updated });
-  //   }} />
-  // </label><br />
-
-  //             <label>Start Date:
-  //               <input type='month' value={we.startDate} onChange={e => {
-  //                 const updated = [...form.workExperience];
-  //                 updated[i].startDate = e.target.value;
-  //                 setForm({ ...form, workExperience: updated });
-  //               }} />
-  //             </label><br />
-  //             <label>End Date:
-  //               <input type='month' value={we.endDate} onChange={e => {
-  //                 const updated = [...form.workExperience];
-  //                 updated[i].endDate = e.target.value;
-  //                 setForm({ ...form, workExperience: updated });
-  //               }} />
-  //             </label><br />
-  //             <label>Achievements:</label><br />
-  //             {we.achievements.map((ach, j) => (
-  //               <input key={j} value={ach} onChange={e => updateWorkAchievement(i, j, e.target.value)} />
-  //             ))}
-  //             <button type="button" onClick={() => {
-  //               const updated = [...form.workExperience];
-  //               updated[i].achievements.push('');
-  //               setForm({ ...form, workExperience: updated });
-  //             }}>+ Add Achievement</button>
-  //           </div>
-  //         ))}
-  //         <button type="button" onClick={addWorkExperience}>+ Add Work Experience</button>
-  //       </fieldset>
-
-  //       <fieldset>
-  //         <legend>Portfolio</legend>
-  //         <label>LinkedIn:
-  //           <input value={form.portfolio.socialLinks.linkedin} onChange={e => {
-  //             const updated = { ...form.portfolio };
-  //             updated.socialLinks.linkedin = e.target.value;
-  //             setForm({ ...form, portfolio: updated });
-  //           }} />
-  //         </label><br />
-  //         <label>Website:
-  //           <input value={form.portfolio.socialLinks.personalPortfolioWebsite} onChange={e => {
-  //             const updated = { ...form.portfolio };
-  //             updated.socialLinks.personalPortfolioWebsite = e.target.value;
-  //             setForm({ ...form, portfolio: updated });
-  //           }} />
-  //         </label><br />
-  //         <label>Additional Link:
-  //           <input value={form.portfolio.socialLinks.additionalLinks[0] || ''} onChange={e => {
-  //             const updated = { ...form.portfolio };
-  //             updated.socialLinks.additionalLinks[0] = e.target.value;
-  //             setForm({ ...form, portfolio: updated });
-  //           }} />
-  //         </label>
-  //       </fieldset>
-
-  // <fieldset>
-  //   <legend>Desired Job Titles</legend>
-  //   {form.jobPreference.desiredJobTitle.map((title, i) => (
-  //     <div key={i}>
-  //       <input
-  //         value={title}
-  //         onChange={e => {
-  //           const updated = [...form.jobPreference.desiredJobTitle];
-  //           updated[i] = e.target.value;
-  //           setForm(prev => ({
-  //             ...prev,
-  //             jobPreference: {
-  //               ...prev.jobPreference,
-  //               desiredJobTitle: updated
-  //             }
-  //           }));
-  //         }}
-  //       />
-  //     </div>
-  //   ))}
-  //   <button
-  //     type="button"
-  //     onClick={() =>
-  //       setForm(prev => ({
-  //         ...prev,
-  //         jobPreference: {
-  //           ...prev.jobPreference,
-  //           desiredJobTitle: [...prev.jobPreference.desiredJobTitle, '']
-  //         }
-  //       }))
-  //     }
-  //   >
-  //     + Add Desired Job Title
-  //   </button>
-  // </fieldset>
-  // <fieldset>
-  //   <legend>Salary Expectation</legend>
-
-  //   <label>Minimum Salary:
-  //     <input
-  //       type="number"
-  //       value={form.jobPreference.salaryExpectation.min}
-  //       onChange={e =>
-  //         setForm(prev => ({
-  //           ...prev,
-  //           jobPreference: {
-  //             ...prev.jobPreference,
-  //             salaryExpectation: {
-  //               ...prev.jobPreference.salaryExpectation,
-  //               min: parseInt(e.target.value || 0)
-  //             }
-  //           }
-  //         }))
-  //       }
-  //     />
-  //   </label><br />
-
-  //   <label>
-  //     <input
-  //       type="checkbox"
-  //       checked={form.jobPreference.salaryExpectation.perHour}
-  //       onChange={e =>
-  //         setForm(prev => ({
-  //           ...prev,
-  //           jobPreference: {
-  //             ...prev.jobPreference,
-  //             salaryExpectation: {
-  //               ...prev.jobPreference.salaryExpectation,
-  //               perHour: e.target.checked
-  //             }
-  //           }
-  //         }))
-  //       }
-  //     />
-  //     Per Hour
-  //   </label><br />
-
-  //   <label>
-  //     <input
-  //       type="checkbox"
-  //       checked={form.jobPreference.salaryExpectation.perYear}
-  //       onChange={e =>
-  //         setForm(prev => ({
-  //           ...prev,
-  //           jobPreference: {
-  //             ...prev.jobPreference,
-  //             salaryExpectation: {
-  //               ...prev.jobPreference.salaryExpectation,
-  //               perYear: e.target.checked
-  //             }
-  //           }
-  //         }))
-  //       }
-  //     />
-  //     Per Year
-  //   </label><br />
-  // </fieldset>
-
-
-  //       <label>Job Type:
-  //   <select
-  //     value={form.jobPreference.jobType}
-  //     onChange={e => handleNestedChange('jobPreference', 'jobType', e.target.value)}
-  //   >
-  //     <option value="">Select job type</option>
-  //     <option value="Remote">Remote</option>
-  //     <option value="full-time">full-time</option>
-  //     <option value="part-time">part-time</option>
-  //     <option value="contract">contract</option>
-  //     <option value="internship">internship</option>
-  //     <option value="on-site">on-site</option>
-  //     <option value="hybrid">hybrid</option>
-  //   </select>
-  // </label><br />
-
-
-  //       <button onClick={submitForm}>Submit</button>
-  //     </Container>
-  //   );
 }
