9aabe7a921538411333ed5647bbe1eca3138f7e0
Sindy Montano <sindymontano@Sindys-MacBook-Air.local>
2025-07-02 15:14:32 -0700

    Adding .catch code.

--- frontend/src/pages/employer/dashboardSections/PopularTech.jsx
+++ frontend/src/pages/employer/dashboardSections/PopularTech.jsx
@@ -2,9 +2,7 @@ import { useEffect, useState } from "react";
 import { candidateApi } from "../../../services/api";
 
 const PopularTech = () => {
-
     const [roles, setRoles] = useState([]);
-
     useEffect( () => {
         candidateApi.getDashboardMainRoleCounts()
         .then( result => {
@@ -12,29 +10,31 @@ const PopularTech = () => {
             // console.log(roles);
             
         })
-        .catch();
+        .catch( error => {
+            console.log(error);
+            }
+        );
 
     }, []);
 
     return (
         <div className="container">
             <h4>Popular Technologies (As per interested Candidates)</h4>
-        <div className="d-flex justify-content-lg-around flex-wrap gap-6">
-            {roles.map((role) => (
-                    <div className="card">
-                        <div className="card-body">
-                            <div><span>{role.count}</span></div>
-                            <div>
-                                <span>“{role.role}”</span>
-                                <span>Candidates</span>
+            <div className="d-flex justify-content-lg-around flex-wrap gap-6">
+                {roles.map((role) => (
+                        <div className="card">
+                            <div className="card-body">
+                                <div><span>{role.count}</span></div>
+                                <div>
+                                    <span>“{role.role}”</span>
+                                    <span>Candidates</span>
+                                </div>
                             </div>
                         </div>
-                    </div>
-                ))}
-        </div>
+                    ))}
+            </div>
         </div>
     );
-
 };
 
 export default PopularTech;
