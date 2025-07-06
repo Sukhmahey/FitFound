48dcf02e8786ba5b726cc900d0178ce12d8a1860
Hitin Sachdeva <140743627+HitinSachdeva161@users.noreply.github.com>
2025-07-05 20:54:34 -0700

    notificationUtilityForFrontEnd

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -18,104 +18,97 @@ import Unauthorized from "./pages/Unauthorized";
 import ExperienceVerificationReqPage from "./pages/candidate/ExperienceVerificationReqPage";
 import MainLayout from "./components/MainLayout";
 import SettingsPage from "./pages/candidate/SettingsPage";
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 function App() {
   return (
-    <Routes>
-      {/* Public Routes */}
-      <Route path="/login" element={<Login />} />
-      <Route path="/signup" element={<Signup />} />
-      {/* <Route path="/unauthorized" element={<Unauthorized />} /> */}
-
-      {/* Candidate Routes */}
-      <Route
-        path="/candidate"
-        element={
-        <ProtectedRoute role="candidate">
-          <MainLayout />
-        </ProtectedRoute>
-        }
-      >
+    <>
+      <Routes>
+        {/* Public Routes */}
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<Signup />} />
+        {/* <Route path="/unauthorized" element={<Unauthorized />} /> */}
+        {/* Candidate Routes */}
         <Route
-          path="onboarding"
+          path="/candidate"
           element={
-            <CandidateOnboarding />
-          }
-        />
-
-        <Route
-          path="dashboard"
-          element={
-            <CandidateDashboard />
-          }
-        />
-
-        <Route
-          path="/candidate/profile"
-          element={
-            <MyProfile></MyProfile>
-          }
-        />
-
-        <Route
-          path="/candidate/connections"
-          element={
-            <CandidateConnections />
-          }
-        />
-
-        <Route
-          path="/candidate/badge-verification"
-          element={
-            <ExperienceVerificationReqPage></ExperienceVerificationReqPage>
-          }
-        />
-        <Route
-          path="/candidate/setting"
-          element={
-            <SettingsPage></SettingsPage>
-          }
-        />
-
-      </Route>
-
-      {/* Employer Routes */}
-      <Route
-        path="/employer"
-        element={
-          <ProtectedRoute role="employer">
+          <ProtectedRoute role="candidate">
             <MainLayout />
           </ProtectedRoute>
-        }
-      >
-        <Route path="onboarding" element={<EmployerOnboarding />} />
-
-        <Route path="profile" element={<EmployerProfile />} />
-
-        <Route path="dashboard" element={<EmployerDashboard />} />
-
-        <Route path="search" element={<EmployerSearch />} />
-
-        <Route path="connections" element={<EmployerConnections />} />
-
+          }
+        >
+          <Route
+            path="onboarding"
+            element={
+              <CandidateOnboarding />
+            }
+          />
+          <Route
+            path="dashboard"
+            element={
+              <CandidateDashboard />
+            }
+          />
+          <Route
+            path="/candidate/profile"
+            element={
+              <MyProfile></MyProfile>
+            }
+          />
+          <Route
+            path="/candidate/connections"
+            element={
+              <CandidateConnections />
+            }
+          />
+          <Route
+            path="/candidate/badge-verification"
+            element={
+              <ExperienceVerificationReqPage></ExperienceVerificationReqPage>
+            }
+          />
+          <Route
+            path="/candidate/setting"
+            element={
+              <SettingsPage></SettingsPage>
+            }
+          />
+        </Route>
+        {/* Employer Routes */}
         <Route
-          path="searchResults"
-          element={<EmployerSearchResults />
+          path="/employer"
+          element={
+            <ProtectedRoute role="employer">
+              <MainLayout />
+            </ProtectedRoute>
+          }
+        >
+          <Route path="onboarding" element={<EmployerOnboarding />} />
+          <Route path="profile" element={<EmployerProfile />} />
+          <Route path="dashboard" element={<EmployerDashboard />} />
+          <Route path="search" element={<EmployerSearch />} />
+          <Route path="connections" element={<EmployerConnections />} />
+          <Route
+            path="searchResults"
+            element={<EmployerSearchResults />
+            }
+          />
+        </Route>
+        <Route
+          path="/employer/create-form"
+          element={
+            <ProtectedRoute role="employer">
+              <EmployerJobForm />
+            </ProtectedRoute>
           }
         />
-      </Route>
-
-      <Route
-        path="/employer/create-form"
-        element={
-          <ProtectedRoute role="employer">
-            <EmployerJobForm />
-          </ProtectedRoute>
-        }
-      />
-
       
-    </Routes>
+      </Routes>
+      <ToastContainer position="top-right" autoClose={3000} hideProgressBar />
+    </>
+
+    
   );
 }
 
