2389e06d8e8abf17a1f309260e4887ae43070b15
Sindy Montano <sindymontano@Sindys-MacBook-Air.local>
2025-07-09 13:45:56 -0700

    candidate/insights: creating Insights componente

---
+++ frontend/src/pages/candidate/Insights.jsx
@@ -0,0 +1,19 @@
+// import ProfileVisibility from "./insightsSections/ProfileVisibility";
+// import RecommendedActions from "./insightsSections/RecommendedActions";
+// import SuggestionBoard from "./insightsSections/SuggestionBoard";
+// import AppearanceIn from "./insightsSections/AppearanceIn";
+
+const Insights = () => {
+    return (
+        <div>
+            <h1>Hi!</h1>
+            {/* <ProfileVisibility></ProfileVisibility>
+            <AppearanceIn></AppearanceIn>
+            <RecommendedActions></RecommendedActions>
+            <SuggestionBoard></SuggestionBoard> */}
+        </div>
+    );
+
+};
+
+export default Insights;
