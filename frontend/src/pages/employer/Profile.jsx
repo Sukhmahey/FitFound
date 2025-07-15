1bdb5419939456a6863f581db1f7fb400f35e299
Sindy Montano <sindymontano@Sindys-MacBook-Air.local>
2025-07-15 15:33:20 -0700

    Employer/Profile: Setting required fields. Showing error message when a required field is missning.

--- frontend/src/pages/employer/Profile.jsx
+++ frontend/src/pages/employer/Profile.jsx
@@ -1,7 +1,8 @@
 import { useEffect, useState, useContext } from "react";
 import { useNavigate } from 'react-router-dom';
 import { useForm, FormProvider } from "react-hook-form";
-import { Box, Typography, Button, Paper } from "@mui/material";
+import { Box, Button, Paper, Snackbar } from "@mui/material";
+import MuiAlert from "@mui/material/Alert";
 
 import { employerApi } from "../../services/api";
 import { useAuth } from "../../contexts/AuthContext";
@@ -10,6 +11,7 @@ import { AppInfoContext } from "../../contexts/AppInfoContext";
 
 import CompanyInfo from "./onboardingSteps/CompanyInfo";
 import UserContactInfo from "./onboardingSteps/UserContactInfo";
+import useNotify from "../../utils/notificationService";
 
 const EmployerProfile = () => {
   const navigate = useNavigate();
@@ -22,6 +24,11 @@ const EmployerProfile = () => {
   const [userProfile, setUserProfile] = useState({});
   const [message, setMessage] = useState("");
   const [messageClass, setMessageClass] = useState("");
+  const [formErrors, setFormErrors] = useState({});
+  const [snackOpen, setSnackOpen] = useState(false);
+  const [snackMessage, setSnackMessage] = useState("");
+  const [snackSeverity, setSnackSeverity] = useState("error");
+  const notify = useNotify();
 
   const { setAppGeneralInfo } = useContext(AppInfoContext);
 
@@ -55,7 +62,86 @@ const EmployerProfile = () => {
       });
   }, []);
 
+  const validateForm = (data) => {
+    const errors = {};
+
+    if (formSection === "details") {
+      if(!data.companyLogo)
+        errors.companyLogo = "Company logo image is required";
+      if (!data.companyName?.trim())
+        errors.companyName = "Company name is required";
+      if (
+        !data.establishedYear ||
+        data.establishedYear < 1900 ||
+        data.establishedYear > new Date().getFullYear()
+      ) {
+        errors.establishedYear =
+          "Established year must be between 1900 and current year";
+      }
+      if (!data.businessRegisteredNumber?.trim())
+        errors.businessRegisteredNumber = "Business Reg. Number is required";
+      if (!data.industrySector?.trim())
+        errors.industrySector = "Industry sector is required";
+      if (!data.companySize?.trim())
+        errors.companySize = "Company size is required";
+      if (
+        ![
+          "1-10",
+          "11-50",
+          "51-200",
+          "201-500",
+          "501-1000",
+          "1001-5000",
+          "5000+",
+        ].includes(data.companySize)
+      ) {
+        errors.companySize = "Invalid company size option";
+      }
+      if (!data.workLocation?.trim())
+        errors.workLocation = "Work location is required";
+      if (!data.companyWebsite?.trim())
+        errors.companyWebsite = "Company website is required";
+      if (
+        !data.companyDescription?.trim() ||
+        data.companyDescription.length < 10
+      ) {
+        errors.companyDescription =
+          "Description must be at least 10 characters";
+      }
+    }
+
+    if (formSection === "contact") {
+      if (!data.profilePicture) errors.profilePicture = "Profile picture image is required";
+      if (!data.firstName?.trim()) errors.firstName = "First name is required";
+      if (!data.lastName?.trim()) errors.lastName = "Last name is required";
+      if (!data.phone?.trim()) errors.phone = "Phone number is required";
+      if (!data.email?.trim()) errors.email = "Email is required";
+      if (!data.designation?.trim())
+        errors.designation = "Designation is required";
+      if (
+        data.linkedInProfile?.trim() &&
+        !/^https?:\/\/(www\.)?linkedin\.com\/.*$/.test(data.linkedInProfile)
+      ) {
+        errors.linkedInProfile = "Invalid LinkedIn URL";
+      }
+    }
+
+    return errors;
+  };
+
   const onSubmit = (data) => {
+    const errors = validateForm(data);
+    
+    if (Object.keys(errors).length > 0) {
+      setFormErrors(errors);
+      const messageText = Object.values(errors).join(" , ");
+      setSnackMessage(messageText);
+      setSnackSeverity("error");
+      setSnackOpen(true);
+      return;
+    }
+    setFormErrors({});
+
     let employerProfile;
 
     if (formSection === "details") {
@@ -239,6 +325,22 @@ const EmployerProfile = () => {
           </Paper>
         </form>
       </FormProvider>
+
+      <Snackbar
+        open={snackOpen}
+        autoHideDuration={5000}
+        onClose={() => setSnackOpen(false)}
+      >
+        <MuiAlert
+          onClose={() => setSnackOpen(false)}
+          severity={snackSeverity}
+          sx={{ width: "100%" }}
+          elevation={6}
+          variant="filled"
+        >
+          {snackMessage}
+        </MuiAlert>
+      </Snackbar>
     </Box>
   );
 };
